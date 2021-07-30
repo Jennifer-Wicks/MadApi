d2b893b4f345dc934d65b9d42cc29c14c489dcae
boneswvb <boneswvb@gmail.com>
2021-07-30 13:43:53 +0200

    Dotenv update for password security updated

--- Index.js
+++ Index.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const apiRoute = require('./routes/api/index');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 8000
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
 app.use(cors())
-
 app.use('/api', apiRoute);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('App is listening on port 5000');
 })
 
--- database/Index.js
+++ database/Index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: 'happyfeet3*',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     port: 5432,
-    database: 'madbookings'
+    database: process.env.DB_NAME
   }
 });
 
