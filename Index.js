df58bfb7ce73f6e4d09274f664f9d540b5713c77
boneswvb <boneswvb@gmail.com>
2021-11-16 09:45:56 +0200

    2021-11-16

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const apiRoute = require('./routes/api/index');
 const cors = require('cors');
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
